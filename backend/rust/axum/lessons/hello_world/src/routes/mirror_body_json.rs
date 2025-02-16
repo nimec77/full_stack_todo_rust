d83cfa111fa80ec543f7123d7da98ad56b528b22
Dmitry Seloustev <d.seloustev@adguard.com>
2025-02-16 15:58:32 +0700

    Complete lesson: 'Extraction a JSON - Introduction to Axum 0.5'

---
+++ backend/rust/axum/lessons/hello_world/src/routes/mirror_body_json.rs
@@ -0,0 +1,22 @@
+use axum::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MirrorJson {
+    message: String,
+}
+
+#[derive(Serialize)]
+pub struct MirrorJsonResponse {
+    message: String,
+    message_from_server: String,
+}
+
+pub async fn mirror_body_json(Json(body): Json<MirrorJson>) -> Json<MirrorJsonResponse> {
+    dbg!(&body);
+    
+    Json(MirrorJsonResponse {
+        message: body.message,
+        message_from_server: "Hello from server".to_owned(),
+    })
+}
