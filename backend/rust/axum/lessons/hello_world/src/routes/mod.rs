83a3117ac1955881fd4a7a39bc37a9e1d2dd5b30
Dmitry Seloustev <d.seloustev@adguard.com>
2025-02-19 21:30:46 +0700

    Complete lesson: 'Custom Muddleware - Introduction to Axum 0.5'

--- backend/rust/axum/lessons/hello_world/src/routes/mod.rs
+++ backend/rust/axum/lessons/hello_world/src/routes/mod.rs
@@ -6,7 +6,10 @@ mod query_params;
 mod mirror_user_agent;
 mod mirror_custom_header;
 mod middleware_message;
-use axum::{http::Method, routing::{get, post}, Extension, Router};
+mod read_middleware_custom_header;
+mod set_middleware_custom_header;
+
+use axum::{http::Method, middleware, routing::{get, post}, Extension, Router};
 use tower_http::cors::{Any, CorsLayer};
 
 use hello_world::hello_world;
@@ -17,6 +20,8 @@ use query_params::query_params;
 use mirror_user_agent::mirror_user_agent;
 use mirror_custom_header::mirror_custom_header;
 use middleware_message::middleware_message;
+use read_middleware_custom_header::read_middleware_custom_header;
+use set_middleware_custom_header::set_middleware_custom_header;
 
 #[derive(Clone)]
 pub struct SharedData {
@@ -33,6 +38,8 @@ pub fn create_routes() -> Router {
     };
 
     Router::new()
+        .route("/read_middleware_custom_header", get(read_middleware_custom_header))
+        .route_layer(middleware::from_fn(set_middleware_custom_header))
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
---
+++ backend/rust/axum/lessons/hello_world/src/routes/set_middleware_custom_header.rs
@@ -0,0 +1,23 @@
+use axum::{
+    extract::Request,
+    http::{HeaderMap, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+
+use super::read_middleware_custom_header::HeaderMessage;
+
+pub async fn set_middleware_custom_header(
+    headers: HeaderMap,
+    mut request: Request,
+    next: Next,
+) -> Result<Response, StatusCode> {
+    let message = headers
+        .get("x-message")
+        .ok_or_else(|| StatusCode::BAD_REQUEST)?;
+    let message = message.to_str().map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    request.extensions_mut().insert(HeaderMessage(message.to_owned()));
+
+    Ok(next.run(request).await)
+}
