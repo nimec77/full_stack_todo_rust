736b9927a2916c1281ec1a2affb58da76c78e7af
Dmitry Seloustev <d.seloustev@adguard.com>
2025-02-23 21:40:48 +0700

    Complete lesson: 'Selecting One Item from the Database - Introduction to Axum 0.5'

--- backend/rust/axum/project_solution/src/router.rs
+++ backend/rust/axum/project_solution/src/router.rs
@@ -2,11 +2,12 @@
 use axum::{routing::{get, post}, Extension, Router};
 use sea_orm::DatabaseConnection;
 
-use crate::routes::{hello_world::hello_world, tasks::create_task::create_task};
+use crate::routes::{hello_world::hello_world, tasks::{create_task::create_task, get_one_task::get_one_task}};
 
 pub fn create_router(database: DatabaseConnection) -> Router {
     Router::new()
         .route("/", get(hello_world))
-        .route("/create_task", post(create_task))
+        .route("/task", post(create_task))
+        .route("/task/{id}", get(get_one_task))
         .layer(Extension(database))
 }
