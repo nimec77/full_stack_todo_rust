ff2612a848baf702fd9d50b919ad9dab071d9400
Dmitry Seloustev <d.seloustev@adguard.com>
2025-03-22 22:18:37 +0700

    'Creating Tasks - Introduction to Axum 0.6 Project' t9:43

--- backend/rust/axum/project_solution/src/routes/tasks/create_task.rs
+++ backend/rust/axum/project_solution/src/routes/tasks/create_task.rs
@@ -1,15 +1,12 @@
-use axum::{extract::State, http::StatusCode, Extension, Json};
+use axum::{Extension, Json, extract::State, http::StatusCode};
 use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr, Set};
-use serde::Deserialize;
 
-use crate::{database::{tasks, users::Model}, errors::app_error::AppError};
+use crate::{
+    database::{tasks, users::Model},
+    errors::app_error::AppError,
+};
 
-#[derive(Debug, Deserialize)]
-pub struct RequestTask {
-    title: String,
-    description: Option<String>,
-    priority: Option<String>,
-}
+use super::RequestTask;
 
 pub async fn create_task(
     State(db): State<DatabaseConnection>,
@@ -17,17 +14,16 @@ pub async fn create_task(
     Json(request_task): Json<RequestTask>,
 ) -> Result<StatusCode, AppError> {
     let new_task = tasks::ActiveModel {
-        priority: Set(request_task.priority),
-        title: Set(request_task.title),
-        description: Set(request_task.description),
+        priority: Set(request_task.priority.unwrap_or(None)),
+        title: Set(request_task.title.unwrap()),
+        description: Set(request_task.description.unwrap_or(None)),
         user_id: Set(Some(user.id)),
         ..Default::default()
     };
 
-    new_task
-        .save(&db)
-        .await
-        .map_err(|_: DbErr| AppError::new(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"))?;
+    new_task.save(&db).await.map_err(|_: DbErr| {
+        AppError::new(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+    })?;
 
     Ok(StatusCode::CREATED)
 }
--- backend/rust/axum/project_solution/src/routes/tasks/mod.rs
+++ backend/rust/axum/project_solution/src/routes/tasks/mod.rs
@@ -1,5 +1,50 @@
 pub mod create_task;
 pub mod delete_task;
 pub mod get_tasks;
-pub mod update_task;
 pub mod partial_update_task;
+pub mod update_task;
+
+use chrono::{DateTime, FixedOffset};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct RequestTask {
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        with = "::serde_with::rust::double_option"
+    )]
+    pub priority: Option<Option<String>>,
+    pub title: Option<String>,
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        with = "::serde_with::rust::double_option"
+    )]
+    pub description: Option<Option<String>>,
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        with = "::serde_with::rust::double_option"
+    )]
+    pub completed_at: Option<Option<DateTime<FixedOffset>>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ResponseTask {
+    pub id: i32,
+    pub title: String,
+    pub description: Option<String>,
+    pub priority: Option<String>,
+    pub completed_at: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ResponseDataTask {
+    pub task: ResponseTask,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ResponseDataTaskList {
+    pub tasks: Vec<ResponseTask>,
+}
